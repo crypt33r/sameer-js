17677a924561c0bdc2d600327a6c3b2aa60d4831
crypt3r <105145560+crypt33r@users.noreply.github.com>
2024-06-14 05:15:07 +0000

    container config

---
+++ Basics_js/variables.js
@@ -0,0 +1,8 @@
+const accountId = 12345
+let accountEmail = "sameer@facebook.com"
+var accountPassword = "121212"
+accountPassword = "Kathmandu"
+let accountState = ""
+
+// console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountState]);
